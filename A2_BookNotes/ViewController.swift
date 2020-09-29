3fb502f0c1cf780da5ec355deeca53a54599d2ee
JiayuYuan <s3692911@student.rmit.edu.au>
2020-09-30 00:07:16 +0800

    Initial Commit

---
+++ A2_BookNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_BookNotes
+//
+//  Created by Jiayu's on 30/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
